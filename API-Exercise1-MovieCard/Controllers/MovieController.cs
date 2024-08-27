77e897fa5c6cc83d58ffdb1c6bdc9a29e07b775d
RikiRhen <riki.rhen@live.se>
2024-08-27 09:53:00 +0200

    Post and Put, first iterations

--- API-Exercise1-MovieCard/Controllers/MovieController.cs
+++ API-Exercise1-MovieCard/Controllers/MovieController.cs
@@ -7,22 +7,23 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using API_Exercise1_MovieCard.Data;
 using API_Exercise1_MovieCard.Models.DTOs;
+using API_Exercise1_MovieCard.Models.Entities;
 
 namespace API_Exercise1_MovieCard.Controllers
 {
-    [Route("api/Movies")]
+    [Route("api/")]
     [ApiController]
-    public class MovieDtoController : ControllerBase
+    public class MovieController : ControllerBase
     {
         private readonly MovieCardContext _context;
 
-        public MovieDtoController(MovieCardContext context)
+        public MovieController(MovieCardContext context)
         {
             _context = context;
         }
 
         //GET: api/Movies
-        [HttpGet]
+        [HttpGet("Movies")]
         public async Task<ActionResult<IEnumerable<MovieDto>>> GetMovies()
         {
             var dto = _context.Movie.Select(m => new MovieDto(m.Id, m.Title, m.Rating, m.ReleaseDate, m.Description, m.Genres.Select(g => g.GenreName).ToList(), m.Actors.Select(a => a.Name).ToList(), m.Director.Name));
@@ -30,7 +31,7 @@ namespace API_Exercise1_MovieCard.Controllers
         }
 
         //GET: api/Movies/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetMovie")]
         public async Task<ActionResult<MovieDto>> GetMovie(int id)
         {
             var dto = await _context.Movie
@@ -54,6 +55,53 @@ namespace API_Exercise1_MovieCard.Controllers
             }
 
             return Ok(dto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Movie>> CreateMovie(MovieForCreationDto newMovie)
+        {
+            if (newMovie == null)
+            {
+                return BadRequest(); 
+            }
+
+            var finalMovieToAdd = new Movie()
+            {
+                Title = newMovie.Title,
+                Rating = newMovie.Rating,
+                ReleaseDate = newMovie.ReleaseDate,
+                Description = newMovie.Description,
+                Genres = newMovie.Genres,
+                Actors = newMovie.Actors,
+                Director = newMovie.Director
+            };
+
+            _context.Movie.Add(finalMovieToAdd);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute("GetMovie", finalMovieToAdd);
+        }
+
+        [HttpPut("{movieId}")]
+        public async Task<ActionResult> UpdateMovie(int movieId, MovieForUpdateDto updateMovie)
+        {
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == movieId);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+
+            movie.Title = updateMovie.Title;
+            movie.Rating = updateMovie.Rating;
+            movie.ReleaseDate = updateMovie.ReleaseDate;
+            movie.Description = updateMovie.Description;
+            movie.Genres = updateMovie.Genres;
+            movie.Actors = updateMovie.Actors;
+            movie.Director = updateMovie.Director;
+
+            return NoContent();
+
         }
 
 
---
+++ API-Exercise1-MovieCard/Models/DTOs/MovieForUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using API_Exercise1_MovieCard.Models.Entities;
+
+namespace API_Exercise1_MovieCard.Models.DTOs
+{
+    public class MovieForUpdateDto
+    {
+        public string Title { get; set; }
+        public int Rating { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Description { get; set; }
+        public int DirectorId { get; set; }
+
+        //Relationships -> 1:M Director, N:M Actor, N:M Genre
+        //Navigation
+        public Director Director { get; set; }
+        public ICollection<Actor> Actors { get; set; } = new List<Actor>();
+        public ICollection<Genre> Genres { get; set; } = new List<Genre>();
+    }
+}
